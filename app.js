fd6f371f86eec69f9fedf9f03aa6c616f326c619
cyberstrong <108492938+syntaCorp@users.noreply.github.com>
2022-11-09 16:59:42 +0000

    job filtering project initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//add color to left of new and featured items
+let newAndFeatured = document.getElementsByClassName("new-featured-post")[0];
+
+// array.forEach(newAndFeatured => {
+  
+// });
+
+const newFeaturedStyling = () => {
+    newAndFeatured.style.borderLeft = "10px solid #5CA5A5"
+}
+
+
+
+// let newAndFeatured = document.getElementById("new-featured-post");
